7a86304532d9d0cce503debc2ec2b61660793c73
navruzx <nnavruz095@gmail.com>
2024-07-22 17:47:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+// // let a = 5;
+// // let b = 8;
+// // function zoo() {
+// //     console.log(a*b)
+// // };
+// // function okk() {
+// //     console.log(a/b);
+// // };
+// // function asa() {
+// //     console.log(a-b);
+// // };
+// // function pop() {
+// //     console.log(a+b);
+// // // };
+// // function plus(a, b) {
+// //     alert(a + b);
+// // }
+// // function minus(a, b) {
+// //     alert(a - b);
+// // }
+// // function div(a, b) {
+// //     alert(a / b);
+// // }
+// // function mult(a, b) {
+// //     alert(a * b);
+// // }
+// // function calculator() {
+// //     let num = +prompt("1-son");
+// //     let num2 = +prompt("2-son");
+// //     let amal = prompt("Amalni kirit");
+// //     switch (amal) {
+// //         case "+":
+// //             plus(num, num2);
+// //             break;
+// //             case "-":
+// //             minus(num, num2);
+// //             break;
+// //             case "/":
+// //                 div(num, num2);
+// //                 break;
+// //                 case "/":
+// //                     break;
+// //                     case "*":
+// //                         mult(num, num2);
+// //                         break;
+// //     };
+
+// //     };
+
+// // calculator();
+
+// function cars(name,model,color,price) {
+// console.log(`Mashinani ismi ${name}`);
+// console.log(`Mashinani madeli ${model}`);
+// console.log(`Mashinani rangi ${color}`);
+// console.log(`Mashinani narxi ${price}`);
+// }
+// cars();
+// function powerA3() {
+        //  let num = +prompt("Son kirit");
+        //  alert(num**3)
+// }
+// powerA3()
+// function powerA234(a) {
+// console.log(a**2);
+// console.log(a**3);
+// console.log(a**4);
+// };
+// powerA234(8 )
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
